b7b30382ea5d000fb34e4a0cef7322ab619bd630
olgashumilova <volga_shumilova@mail.ru>
2021-05-03 14:18:33 +0300

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+import MainPage from './components/MainPage'
+import GoodsPage from './components/GoodsPage'
+import AddressesPage from './components/AddressesPage'
+import CartPage from './components/CartPage'
+
+import './source/scss/components/_header.scss'
+import './source/scss/components/_footer.scss'
+
+import './source/scss/components/MainPage/_main.scss'
+import './source/scss/components/GoodsPage/_goodspage.scss'
+import './source/scss/components/AddressesPage/_addressespage.scss'
+// import './source/scss/components/CartPage/_cartpage.scss'
+
+import './source/scss/config/_reset.scss'
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//       <MainPage />
+//       <GoodsPage />
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <div>
+
+      <header className = 'header'>
+
+        <div className = 'header__container'>
+
+          <h1 className = 'title container'>ULTRAVIOLET</h1>
+          <h2 className = 'container'> <Link to="/">Главная</Link> </h2>
+          <h2 className = 'container'> <Link to="/catalog">Каталог</Link> </h2>
+          <h2 className = 'container'> <Link to="/addresses">Адреса</Link> </h2>
+          <button className = 'header__cart container'> <Link to="/cart"></Link> </button>
+
+        </div>
+
       </header>
-    </div>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            
+        <Switch>
+
+          <Route path="/cart">
+            <Cart />
+          </Route>
+
+          <Route path="/catalog">
+            <Catalog />
+          </Route>
+
+          <Route path="/addresses">
+            <Addresses />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <MainPage />;
+}
+
+function Catalog() {
+  return <GoodsPage />;
+}
+
+function Addresses() {
+  return <AddressesPage />;
+}
+
+function Cart() {
+  return <CartPage />
+}
