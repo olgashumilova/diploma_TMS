5235ae1d2e3cd19b807a17cc1b59ee2ccf08f87e
olgashumilova <volga_shumilova@mail.ru>
2021-06-09 21:02:15 +0300

    feat: add state and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,53 @@
-import { combineReducers } from 'redux';
+import { ACTION_TYPES } from '../const';
 
-import GoodsReducer from './GoodsReducer';
-import ItemReducer from './ItemReducer';
+export const initialState = {
+  cart: [],
+  totalQuantity: 0,
+}
 
-export default combineReducers({
-    goods: GoodsReducer,
-    item: ItemReducer,
-  });
+const reducer = (state = initialState, action) => {
+
+  console.log(state.cart)
+
+  switch (action.type) {
+    
+    case ACTION_TYPES.ADD_ITEM: {
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+        totalQuantity: state.totalQuantity + 1,
+      }
+    }
+    case ACTION_TYPES.DECREASE_ITEM: {
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+        totalQuantity: state.totalQuantity - 1,
+      }
+    }
+    case ACTION_TYPES.INCREASE_ITEM: {
+      return {
+        ...state,
+        cart: [...state.cart, action.payload],
+        totalQuantity: state.totalQuantity + 1,
+      }
+    }
+    case ACTION_TYPES.REMOVE_ITEM: {
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+        totalQuantity: state.totalQuantity - 1,
+      }
+    }
+    case ACTION_TYPES.CLEAR_CART: {
+      return { 
+        ...state, 
+        cart: [],
+        totalQuantity: 0
+      }
+    }
+    default: return state;
+  }
+}
+
+export default reducer
