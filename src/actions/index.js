1f3d08b2a10e049a42ef51991ae89500362e9978
olgashumilova <volga_shumilova@mail.ru>
2021-06-09 21:00:12 +0300

    feat: add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,26 @@
-// export * from './AuthActions';
-export * from './GoodsActions';
-export * from './ItemActions';
+import { ACTION_TYPES } from '../const'
+
+export const addItemToCart = (item) => ({
+    type: ACTION_TYPES.ADD_ITEM,
+    payload: item,
+});
+
+export const removeItemFromCart = (id) => ({
+    type: ACTION_TYPES.REMOVE_ITEM,
+    payload: { id: id },
+});
+
+export const decreaseQuantity = (item) => ({
+    type: ACTION_TYPES.DECREASE_ITEM,
+    payload: item,
+});
+
+export const increaseQuantity = (item) => ({
+    type: ACTION_TYPES.INCREASE_ITEM,
+    payload: item,
+});
+
+export const clearCart = (item) => ({
+    type: ACTION_TYPES.CLEAR_CART,
+    payload: item,
+});
